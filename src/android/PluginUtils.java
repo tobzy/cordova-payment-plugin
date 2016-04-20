e7a6b10a76afab43bf9e35a13309992a24ec0134
Babajide.Apata <babajide.apata@interswitchgroup.com>
2016-04-20 19:31:53 +0100

    Addition of PluginUtils class

---
+++ src/android/PluginUtils.java
@@ -0,0 +1,23 @@
+import org.apache.cordova.CordovaWebView;
+import org.apache.cordova.CallbackContext;
+import org.apache.cordova.CordovaPlugin;
+import org.apache.cordova.CordovaInterface;
+import org.apache.cordova.PluginResult;
+import com.google.gson.Gson;
+
+public class PluginUtils extends CordovaPlugin{
+    public PluginUtils(){
+
+    }
+    public void initialize(CordovaInterface cordova, CordovaWebView webView) {
+        super.initialize(cordova, webView);
+    }
+    public static <T> PluginResult getPluginResult(CallbackContext callbackContext, T response){
+        Gson gson = new Gson();
+        PluginResult result = null;
+        result = new PluginResult(PluginResult.Status.OK, gson.toJson(response));
+        result.setKeepCallback(true);
+        callbackContext.sendPluginResult(result);
+        return result;
+    }
+}
