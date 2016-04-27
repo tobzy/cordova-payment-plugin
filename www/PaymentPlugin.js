b889c44dc736b173addeddd61026e2a7f5063c90
Babajide.Apata <babajide.apata@interswitchgroup.com>
2016-04-27 10:37:43 +0100

    Modification to avoid user having knowledge of the inner classes

--- www/PaymentPlugin.js
+++ www/PaymentPlugin.js
@@ -1,9 +1,9 @@
 var exec = require('cordova/exec');
 function PaymentPlugin() {
- console.log('Payment plugin working');
+ //console.log('Payment plugin working');
 }
-PaymentPlugin.prototype.makePayment = function(action,args){
-   return execPromise(action,args);
+PaymentPlugin.prototype.makePayment = function(args){
+   return execPromise("MakePayment",args);
 }
 
 function execPromise(action, args){
@@ -13,57 +13,57 @@ function execPromise(action, args){
     var defer = new $.Deferred();
      exec(function success(successMessage){
               defer.resolve(successMessage);
-      },
-      function error(errorMessage){
-          defer.reject(errorMessage);
-      },
-      "PaymentPlugin",
-      action,
-      args);
-      return defer.promise();
+    },
+    function error(errorMessage){
+        defer.reject(errorMessage);
+    },
+    "PaymentPlugin",
+    action,
+    args);
+    return defer.promise();
 }
-PaymentPlugin.prototype.init = function(action, args){
-      return execPromise(action,args);
+PaymentPlugin.prototype.init = function(args){
+      return execPromise("Init",args);
  }
- PaymentPlugin.prototype.authorizeOtp = function(action, args){
-       return execPromise(action,args);
-  }
-PaymentPlugin.prototype.loadWallet  = function(action){
-    return execPromise(action);
+ PaymentPlugin.prototype.authorizeOtp = function(args){
+       return execPromise("AuthorizeOTP",args);
+ }
+PaymentPlugin.prototype.loadWallet  = function(){
+    return execPromise("LoadWallet");
 }
 
-PaymentPlugin.prototype.payWithWallet  = function(action, args){
-    return execPromise(action,args);
+PaymentPlugin.prototype.payWithWallet  = function(args){
+    return execPromise("PayWithWallet",args);
 }
-PaymentPlugin.prototype.payWithWalletSDK  = function(action, args){
-    return execPromise(action,args);
-}
-
-PaymentPlugin.prototype.paymentStatus  = function(action, args){
-    return execPromise(action,args);
+PaymentPlugin.prototype.payWithWalletSDK  = function(args){
+    return execPromise("PayWithWalletSDK",args);
 }
 
-PaymentPlugin.prototype.payWithToken  = function(action, args){
-    return execPromise(action,args);
+PaymentPlugin.prototype.paymentStatus  = function(args){
+    return execPromise("PaymentStatus",args);
 }
 
-PaymentPlugin.prototype.validatePaymentCard  = function(action, args){
-    return execPromise(action,args);
-}
-PaymentPlugin.prototype.userInformation = function(action, args){
-    return execPromise(action,args);
+PaymentPlugin.prototype.payWithToken  = function(args){
+    return execPromise("PayWithToken",args);
 }
 
-PaymentPlugin.prototype.validateCard  = function(action, args){
-    return execPromise(action,args);
+PaymentPlugin.prototype.validatePaymentCard  = function(args){
+    return execPromise("ValidatePaymentCard",args);
+}
+PaymentPlugin.prototype.userInformation = function(args){
+    return execPromise("UserInformation",args);
 }
 
-PaymentPlugin.prototype.pay = function(action, args){
-    return execPromise(action,args);
+PaymentPlugin.prototype.validateCard  = function(args){
+    return execPromise("ValidateCard",args);
 }
 
-PaymentPlugin.prototype.payWithCard = function(action, args){
-      return execPromise(action,args);
+PaymentPlugin.prototype.pay = function(args){
+    return execPromise("Pay",args);
+}
+
+PaymentPlugin.prototype.payWithCard = function(args){
+      return execPromise("PayWithCard",args);
  }
 var paymentPlugin = new PaymentPlugin();
-module.exports = paymentPlugin;
+module.exports = new PaymentPlugin();
