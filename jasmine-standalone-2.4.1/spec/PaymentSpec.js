c43791a3756f993dbe88e8fd706c7595e359843d
Babajide.Apata <babajide.apata@interswitchgroup.com>
2016-04-28 11:04:54 +0100

    Test cases with Jasmine

---
+++ jasmine-standalone-2.4.1/spec/PaymentSpec.js
@@ -0,0 +1,33 @@
+describe("initialization of Payment Plugin", function(){
+  var payment;
+  beforeEach(function() {
+    payment = new PaymentPlugin();
+    expect(payment).to
+  });
+
+  it("should be able instantiate Payment Plugin", function(){
+    expect(payment).not.toBe(null);
+  });
+  it("should be able initialize the Plugin class", function(){
+      expect(payment.init).not.toBe(null);
+      expect(payment.init).toBeDefined();
+  });
+});
+
+describe("The test class for Payment Plugin", function(){
+  var payment;
+  beforeEach(function() {
+    payment = new PaymentPlugin();
+  });
+
+  it("should be able to validate card", function(){
+  var validateCardRequest = {
+    customerId : 1234567890 // Optional email, mobile no, BVN etc to uniquely identify the customer
+  };
+  //var validateCard = payment.validatePaymentCard(validateCardRequest);
+    expect(payment.validatePaymentCard).toBeDefined();
+  });
+  it("should be able to initialize the Plugin class", function(){
+      expect(payment.init).not.toBe(null);
+  });
+});
