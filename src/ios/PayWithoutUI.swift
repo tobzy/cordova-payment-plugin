a08830b0638cc85c093b97f473ba6c4e6b99f06c
Efe Ariaroo <efe.ariaroo@interswitchgroup.com>
2016-05-13 12:15:09 +0100

    Refactor of the calls that send the results of processing back to Javascript

--- src/ios/PayWithoutUI.swift
+++ src/ios/PayWithoutUI.swift
@@ -9,7 +9,7 @@ import SwiftyJSON
 
 public class PayWithoutUI {
     
-    class func makePayment(cdvPlugin: PaymentPlugin, cdvCommand: CDVInvokedUrlCommand, thePan: String,
+    class func makePayment(cdvPlugin: PaymentPlugin, command: CDVInvokedUrlCommand, thePan: String,
                            theAmount:String, theCvv: String, thePin: String, theExpiryDate: String,
                            theCustomerId: String, theCurrency: String) {
         let purchaseSdk : PaymentSDK = PaymentSDK(clientId: cdvPlugin.clientId, clientSecret: cdvPlugin.clientSecret)
@@ -22,62 +22,56 @@ public class PayWithoutUI {
         purchaseSdk.purchase(request, completionHandler:{(purchaseResponse: PurchaseResponse?, error: NSError?) in
             guard error == nil else {
                 UIApplication.sharedApplication().networkActivityIndicatorVisible = false
+
                 let errMsg = (error?.localizedDescription)!
-                
-                let pluginResult = CDVPluginResult(status: CDVCommandStatus_ERROR, messageAsString: errMsg)
-                cdvPlugin.commandDelegate!.sendPluginResult(pluginResult, callbackId: cdvCommand.callbackId)
+                Utils.sendErrorBackToJavascript(cdvPlugin, cdvCommand: command, errMsg: errMsg)
                 return
             }
             
             guard let response = purchaseResponse else {
                 UIApplication.sharedApplication().networkActivityIndicatorVisible = false
-                let errMsg = (error?.localizedFailureReason)!
-                
-                let pluginResult = CDVPluginResult(status: CDVCommandStatus_ERROR, messageAsString: errMsg)
-                cdvPlugin.commandDelegate!.sendPluginResult(pluginResult, callbackId: cdvCommand.callbackId)
+
+                let failureMsg = (error?.localizedFailureReason)!
+                Utils.sendErrorBackToJavascript(cdvPlugin, cdvCommand: command, errMsg: failureMsg)
                 return
             }
             //Success or OTP
             UIApplication.sharedApplication().networkActivityIndicatorVisible = false
-            let pluginResult = CDVPluginResult(status: CDVCommandStatus_OK, messageAsString: Utils.getJsonOfPurchaseResponse(response))
-            cdvPlugin.commandDelegate!.sendPluginResult(pluginResult, callbackId: cdvCommand.callbackId)
+            Utils.sendSuccessBackToJavascript(cdvPlugin, cdvCommand: command, successMsg: Utils.getJsonOfPurchaseResponse(response))
         })
     }
     
-    class func loadWallet(cdvPlugin: PaymentPlugin, cdvCommand: CDVInvokedUrlCommand) {
+    class func loadWallet(cdvPlugin: PaymentPlugin, command: CDVInvokedUrlCommand) {
         let walletSdk : WalletSDK = WalletSDK(clientId: cdvPlugin.clientId, clientSecret: cdvPlugin.clientSecret)
         UIApplication.sharedApplication().networkActivityIndicatorVisible = true
         
         walletSdk.getPaymentMethods({ (response: WalletResponse?, error: NSError?) -> Void in
             guard error == nil else {
                 UIApplication.sharedApplication().networkActivityIndicatorVisible = false
+
                 let errMsg = (error?.localizedDescription)!
-                
-                let pluginResult = CDVPluginResult(status: CDVCommandStatus_ERROR, messageAsString: errMsg)
-                cdvPlugin.commandDelegate!.sendPluginResult(pluginResult, callbackId: cdvCommand.callbackId)
+                Utils.sendErrorBackToJavascript(cdvPlugin, cdvCommand: command, errMsg: errMsg)
                 return
             }
             
             guard let walletResponse = response else {
                 UIApplication.sharedApplication().networkActivityIndicatorVisible = false
-                let errMsg = (error?.localizedDescription)!
-                
-                let pluginResult = CDVPluginResult(status: CDVCommandStatus_ERROR, messageAsString: errMsg)
-                cdvPlugin.commandDelegate!.sendPluginResult(pluginResult, callbackId: cdvCommand.callbackId)
+
+                let failureMsg = (error?.localizedFailureReason)!
+                Utils.sendErrorBackToJavascript(cdvPlugin, cdvCommand: command, errMsg: failureMsg)
                 return
             }
             
             //Handling success
             UIApplication.sharedApplication().networkActivityIndicatorVisible = false
-            let paymentMethodsJson = Utils.getJsonOfPaymentMethods(walletResponse.paymentMethods)
             
-            let pluginResult = CDVPluginResult(status: CDVCommandStatus_OK, messageAsString: paymentMethodsJson)
-            cdvPlugin.commandDelegate!.sendPluginResult(pluginResult, callbackId: cdvCommand.callbackId)
+            let paymentMethodsJson = Utils.getJsonOfPaymentMethods(walletResponse.paymentMethods)
+            Utils.sendSuccessBackToJavascript(cdvPlugin, cdvCommand: command, successMsg: paymentMethodsJson)
         })
     }
     
     
-    class func payWithWallet(cdvPlugin: PaymentPlugin, cdvCommand: CDVInvokedUrlCommand, theCustomerId: String,
+    class func payWithWallet(cdvPlugin: PaymentPlugin, command: CDVInvokedUrlCommand, theCustomerId: String,
                              theAmount:String, tokenOfUserSelectedPaymentMethod: String,
                              thePin: String, theCurrency: String, theRequestorId: String) {
         let walletSdk : WalletSDK = WalletSDK(clientId: cdvPlugin.clientId, clientSecret: cdvPlugin.clientSecret)
@@ -90,30 +84,26 @@ public class PayWithoutUI {
         walletSdk.purchase(request, completionHandler:{(purchaseResponse: PurchaseResponse?, error: NSError?) in
             guard error == nil else {
                 UIApplication.sharedApplication().networkActivityIndicatorVisible = false
+
                 let errMsg = (error?.localizedDescription)!
-                
-                let pluginResult = CDVPluginResult(status: CDVCommandStatus_ERROR, messageAsString: errMsg)
-                cdvPlugin.commandDelegate!.sendPluginResult(pluginResult, callbackId: cdvCommand.callbackId)
+                Utils.sendErrorBackToJavascript(cdvPlugin, cdvCommand: command, errMsg: errMsg)
                 return
             }
             
             guard let response = purchaseResponse else {
                 UIApplication.sharedApplication().networkActivityIndicatorVisible = false
-                let errMsg = (error?.localizedFailureReason)!
-                
-                let pluginResult = CDVPluginResult(status: CDVCommandStatus_ERROR, messageAsString: errMsg)
-                cdvPlugin.commandDelegate!.sendPluginResult(pluginResult, callbackId: cdvCommand.callbackId)
+
+                let failureMsg = (error?.localizedFailureReason)!
+                Utils.sendErrorBackToJavascript(cdvPlugin, cdvCommand: command, errMsg: failureMsg)
                 return
             }
             //Success or OTP
             UIApplication.sharedApplication().networkActivityIndicatorVisible = false
-            
-            let pluginResult = CDVPluginResult(status: CDVCommandStatus_OK, messageAsString: Utils.getJsonOfPurchaseResponse(response))
-            cdvPlugin.commandDelegate!.sendPluginResult(pluginResult, callbackId: cdvCommand.callbackId)
+            Utils.sendSuccessBackToJavascript(cdvPlugin, cdvCommand: command, successMsg: Utils.getJsonOfPurchaseResponse(response))
         })
     }
     
-    class func paymentStatus(cdvPlugin: PaymentPlugin, cdvCommand: CDVInvokedUrlCommand,
+    class func paymentStatus(cdvPlugin: PaymentPlugin, command: CDVInvokedUrlCommand,
                              theTransactionRef: String, theAmount:String) {
         let purchaseSdk = PaymentSDK(clientId: cdvPlugin.clientId, clientSecret: cdvPlugin.clientSecret)
         UIApplication.sharedApplication().networkActivityIndicatorVisible = true
@@ -121,30 +111,26 @@ public class PayWithoutUI {
         purchaseSdk.getPaymentStatus(theTransactionRef, amount: theAmount, completionHandler:{(paymentStatusResponse: PaymentStatusResponse?, error: NSError?) in
             guard error == nil else {
                 UIApplication.sharedApplication().networkActivityIndicatorVisible = false
+
                 let errMsg = (error?.localizedDescription)!
-                
-                let pluginResult = CDVPluginResult(status: CDVCommandStatus_ERROR, messageAsString: errMsg)
-                cdvPlugin.commandDelegate!.sendPluginResult(pluginResult, callbackId: cdvCommand.callbackId)
+                Utils.sendErrorBackToJavascript(cdvPlugin, cdvCommand: command, errMsg: errMsg)
                 return
             }
             
             guard let response = paymentStatusResponse else {
                 UIApplication.sharedApplication().networkActivityIndicatorVisible = false
-                let errMsg = (error?.localizedFailureReason)!
-                
-                let pluginResult = CDVPluginResult(status: CDVCommandStatus_ERROR, messageAsString: errMsg)
-                cdvPlugin.commandDelegate!.sendPluginResult(pluginResult, callbackId: cdvCommand.callbackId)
+
+                let failureMsg = (error?.localizedFailureReason)!
+                Utils.sendErrorBackToJavascript(cdvPlugin, cdvCommand: command, errMsg: failureMsg)
                 return
             }
             //Success
             UIApplication.sharedApplication().networkActivityIndicatorVisible = false
-            
-            let pluginResult = CDVPluginResult(status: CDVCommandStatus_OK, messageAsString: Utils.getJsonOfPaymentStatus(response))
-            cdvPlugin.commandDelegate!.sendPluginResult(pluginResult, callbackId: cdvCommand.callbackId)
+            Utils.sendSuccessBackToJavascript(cdvPlugin, cdvCommand: command, successMsg: Utils.getJsonOfPaymentStatus(response))
         })
     }
     
-    class func validateCard(cdvPlugin: PaymentPlugin, cdvCommand: CDVInvokedUrlCommand,
+    class func validateCard(cdvPlugin: PaymentPlugin, command: CDVInvokedUrlCommand,
                             thePan: String, theCvv:String, thePin: String, theExpiryDate: String, theCustomerId: String) {
         let purchaseSdk = PaymentSDK(clientId: cdvPlugin.clientId, clientSecret: cdvPlugin.clientSecret)
         UIApplication.sharedApplication().networkActivityIndicatorVisible = true
@@ -155,30 +141,26 @@ public class PayWithoutUI {
         purchaseSdk.validateCard(validateCardRequest, completionHandler:{(validateCardResponse: ValidateCardResponse?, error: NSError?) in 
             guard error == nil else {
                 UIApplication.sharedApplication().networkActivityIndicatorVisible = false
+
                 let errMsg = (error?.localizedDescription)!
-                
-                let pluginResult = CDVPluginResult(status: CDVCommandStatus_ERROR, messageAsString: errMsg)
-                cdvPlugin.commandDelegate!.sendPluginResult(pluginResult, callbackId: cdvCommand.callbackId)
+                Utils.sendErrorBackToJavascript(cdvPlugin, cdvCommand: command, errMsg: errMsg)
                 return
             }
             
             guard let response = validateCardResponse else {
                 UIApplication.sharedApplication().networkActivityIndicatorVisible = false
-                let errMsg = (error?.localizedFailureReason)!
-                
-                let pluginResult = CDVPluginResult(status: CDVCommandStatus_ERROR, messageAsString: errMsg)
-                cdvPlugin.commandDelegate!.sendPluginResult(pluginResult, callbackId: cdvCommand.callbackId)
+
+                let failureMsg = (error?.localizedFailureReason)!
+                Utils.sendErrorBackToJavascript(cdvPlugin, cdvCommand: command, errMsg: failureMsg)
                 return
             }
             //Success
             UIApplication.sharedApplication().networkActivityIndicatorVisible = false
-            
-            let pluginResult = CDVPluginResult(status: CDVCommandStatus_OK, messageAsString: Utils.getJsonOfPurchaseResponse(response))
-            cdvPlugin.commandDelegate!.sendPluginResult(pluginResult, callbackId: cdvCommand.callbackId)
+            Utils.sendSuccessBackToJavascript(cdvPlugin, cdvCommand: command, successMsg: Utils.getJsonOfPurchaseResponse(response))
         })
     }
     
-    class func authorizeOtp(cdvPlugin: PaymentPlugin, cdvCommand: CDVInvokedUrlCommand, theOtp: String,
+    class func authorizeOtp(cdvPlugin: PaymentPlugin, command: CDVInvokedUrlCommand, theOtp: String,
                             theOtpTransactionId:String, theOtpTransactionRef: String) {
         let otpRequest = AuthorizeOtpRequest(otpTransactionIdentifier: theOtpTransactionId,
                                              otp: theOtp, transactionRef: theOtpTransactionRef)
@@ -187,25 +169,21 @@ public class PayWithoutUI {
         purchaseSdk.authorizeOtp(otpRequest, completionHandler: {(authorizeOtpResponse: AuthorizeOtpResponse?, error: NSError?) in
             guard error == nil else {
                 UIApplication.sharedApplication().networkActivityIndicatorVisible = false
+
                 let errMsg = (error?.localizedDescription)!
-                
-                let pluginResult = CDVPluginResult(status: CDVCommandStatus_ERROR, messageAsString: errMsg)
-                cdvPlugin.commandDelegate!.sendPluginResult(pluginResult, callbackId: cdvCommand.callbackId)
+                Utils.sendErrorBackToJavascript(cdvPlugin, cdvCommand: command, errMsg: errMsg)
                 return
             }
             
             guard authorizeOtpResponse != nil else {
                 UIApplication.sharedApplication().networkActivityIndicatorVisible = false
                 
-                let pluginResult = CDVPluginResult(status: CDVCommandStatus_ERROR, messageAsString: "Otp validation was NOT successful")
-                cdvPlugin.commandDelegate!.sendPluginResult(pluginResult, callbackId: cdvCommand.callbackId)
+                Utils.sendErrorBackToJavascript(cdvPlugin, cdvCommand: command, errMsg: "Otp validation was NOT successful")
                 return
             }
             //OTP successful
             UIApplication.sharedApplication().networkActivityIndicatorVisible = false
-            
-            let pluginResult = CDVPluginResult(status: CDVCommandStatus_OK, messageAsString: "Success")
-            cdvPlugin.commandDelegate!.sendPluginResult(pluginResult, callbackId: cdvCommand.callbackId)
+            Utils.sendSuccessBackToJavascript(cdvPlugin, cdvCommand: command, successMsg: "Success")
         })
     }
 }
